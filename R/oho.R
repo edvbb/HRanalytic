b0fb8622799fee95173fe29ae62094577439f45a
edvbb <edvb@yandex.ru>
2018-01-13 12:56:05 +0300

    3 commit

---
+++ R/oho.R
@@ -0,0 +1,14 @@
+#' Short description of what this does
+#'
+#' Longer description of what this does. Approximately a paragraph.
+#'
+#' @param x The first thing
+#' @param y The second thing.
+#' @return The thing that comes out of this function
+#'
+#' @export (do export this in NAMESPACE)
+oho <- function(x,y) {
+  return (x+y)
+}
+
+
