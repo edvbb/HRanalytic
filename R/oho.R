5e396817981431ed30d627fd49f192f3275f068c
edvbb <edvb@yandex.ru>
2018-01-13 13:29:09 +0300

    3 commit

--- R/oho.R
+++ R/oho.R
@@ -5,7 +5,8 @@
 #' @param x The first thing
 #' @param y The second thing.
 #' @return The thing that comes out of this function
-#'
+#'@examples
+#' oho(1, 10)
 #' @export
 oho <- function(x,y) {
   return (x+y)
