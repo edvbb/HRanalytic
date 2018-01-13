252bfe2ef0b411d7b5dad7a3eb358e60c245521f
edvbb <edvb@yandex.ru>
2018-01-13 12:35:56 +0300

    s commit

--- R/aha.R
+++ R/aha.R
@@ -1,3 +1,11 @@
+#' Short description of what this does
+#'
+#' Longer description of what this does. Approximately a paragraph.
+#'
+#' @param a The thing
+#'
+#' @export (do export this in NAMESPACE)
+
 aha <- function(a) {
   rnorm(a)
 }
